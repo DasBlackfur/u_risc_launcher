dce661a0be31fb59eea01994937762d0252c900b
blackfur <64478051+theblackfurguy@users.noreply.github.com>
2021-05-21 19:57:23 +0200

    Major changes to structure

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+use u_risc_interpreter::{Cpu};
+use std::env;
+mod run;
+mod debug;
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    match args.get(1) {
+        None => print_help(""),
+        Some(command) => {match command.as_str() {
+            "run" => {}
+            "debug" => {}
+            _ => {print_help("")}
+        }}
+    }
+}
+
+
+
+fn print_help(err: &str) {
+    print!("{}", err);
+    println!("U-RISC-LAUNCHER v{} using U-RISC-INTERPRETER v{}\nAvailable commands:\n  run FILENAME\n  debug FILENAME", env!("CARGO_PKG_VERSION"), u_risc_interpreter::get_version());
 }

533d506e45a8209eba8b88ff9219be8f01ba83ca
blackfur <64478051+theblackfurguy@users.noreply.github.com>
2021-06-19 10:22:24 +0200

    Oppsie
    Signed-off-by: blackfur <64478051+theblackfurguy@users.noreply.github.com>

--- src/devices.rs
+++ src/devices.rs
@@ -10,7 +10,7 @@ pub(crate) struct TerminalIn {
 
 impl Device for TerminalOut {
     fn get_address_space(&self) -> (u64, u64) {
-        ()
+        todo!()
     }
 
     fn load(&self, address: u64) -> u64 {
